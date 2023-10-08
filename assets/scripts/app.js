8e3028622a692aa7e85d8e2ecad4bb3a6384405e
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-08 22:00:22 +0300

    loaded analytics.js script dynamically through startAnalytics static method

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -172,6 +172,22 @@ class App {
     finishedProjectsList.setSwitchHandlerFunction(
       activeProjectsList.addProject.bind(activeProjectsList)
     );
+
+    // const someScript = document.createElement('script');
+    // someScript.textContent = 'alert("Hi there!");';
+    // document.head.append(someScript);
+
+    // this.startAnalytics();
+    document
+      .getElementById('start-analytics-btn')
+      .addEventListener('click', this.startAnalytics);
+  }
+
+  static startAnalytics() {
+    const analyticsScript = document.createElement('script');
+    analyticsScript.src = 'assets/scripts/analytics.js';
+    analyticsScript.defer = true;
+    document.head.append(analyticsScript);
   }
 }
 
